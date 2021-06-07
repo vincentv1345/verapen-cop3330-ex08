aff2dc6da02cdeedeab58be798d46327ac2e8e44
Vincent <vincentverapen2012@gmail.com>
2021-06-06 21:01:53 -0400

    Final Commit

--- src/main/java/org/example/getNumbers.java
+++ src/main/java/org/example/getNumbers.java
@@ -4,20 +4,17 @@ public class getNumbers {
     public int getPeople(){
         System.out.println( "How many people? " );
         Scanner z = new Scanner(System.in);
-        int person = z.nextInt();
-        return person;
+        return z.nextInt();
     }
     public int getPizzas(){
         System.out.println("How many pizzas do you have? ");
         Scanner p = new Scanner(System.in);
-        int pizza = p.nextInt();
-        return pizza;
+        return p.nextInt();
     }
     public int getPizzaSlices(){
         System.out.println("How many slices per pizza? ");
         Scanner s = new Scanner(System.in);
-        int slice = s.nextInt();
-        return slice;
+        return s.nextInt();
     }
 
 }
